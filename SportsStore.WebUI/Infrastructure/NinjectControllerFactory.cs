283e8424a0b14341e8af5b5ab38efe2f592405e4
Ken Kovar <ken.kovar@yahoo.com>
2012-09-21 16:17:38 -0500

    Update references to packages obtained via NuGet
    Also add NinjectControllerFactory and infrastructure

---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+    }
+}
